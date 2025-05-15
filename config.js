d9fcc096fe54879274ec7f00505a3ad28b181661
RealStruggleWorld <naktestje.com@gmail.com>
2025-05-16 00:02:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const aiko_AUDIO_SORRY = "https://github.com/RealStruggleWorld/AikoShirasaki/raw
 const aiko_AUDIO_ASLEEP = "https://github.com/RealStruggleWorld/AikoShirasaki/raw/refs/heads/main/source/audio/typing_asleep.mp3";
 
 // 🖼️ Aiko's Image & Icons
-const aiko_ICON_CHAT = "source/icon/aiko.png";              // Chat toggle icon
-const aiko_ICON_MUTE_ON = "source/icon/mute_on.webp";      // Mute icon (on)
-const aiko_ICON_MUTE_OFF = "source/icon/mute_off.webp";    // Mute icon (off)
-const aiko_AVATAR = "source/avatar/avatar aiko.webp";      // Full avatar image
+const aiko_ICON_CHAT = "https://raw.githubusercontent.com/RealStruggleWorld/Media/main/source/icon/aiko.png";              // Chat toggle icon
+const aiko_ICON_MUTE_ON = "https://raw.githubusercontent.com/RealStruggleWorld/Media/main/source/icon/mute_on.webp";      // Mute icon (on)
+const aiko_ICON_MUTE_OFF = "https://raw.githubusercontent.com/RealStruggleWorld/Media/main/source/icon/mute_off.webp";    // Mute icon (off)
+const aiko_AVATAR = "https://raw.githubusercontent.com/RealStruggleWorld/Media/main/source/avatar/avatar%20aiko.webp";      // Full avatar image
 
 // 🌐 Aiko's API Endpoint
 const aiko_API_ENDPOINT = "https://0ee984a4-a553-4cee-926a-abec4eaa95e5-00-3vs2iyxti5vcu.sisko.replit.dev/chat";
